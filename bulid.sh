e51b3cde56f71bc2d465fe40541623c3a5eb207b
tay3223 <tay3223@qq.com>
2024-12-01 17:27:22 +0800

    update

--- bulid.sh
+++ bulid.sh
@@ -3,3 +3,19 @@
 helm package .
 
 helm repo index . --url https://gomessage.github.io/gomessage-helm/
+
+
+#客户端操作
+# helm repo add gomessage https://gomessage.github.io/gomessage-helm
+#
+# helm repo update
+#
+# helm repo search gomessage
+#
+# helm upgrade --install gomessage \
+#  gomessage/gomessage \
+#  --create-namespace \
+#  --namespace default
+#
+# helm pull gomessage/gomessage --untar
+# helm fetch gomessage/gomessage --untar
